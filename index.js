27e3151edd9f59824b3c1d1957226e484b6de71d
Eleni <helena.aslanidou@gmail.com>
2022-05-11 15:28:15 +0200

    Created a method to return the certain genre
    Created a method to return specific genre and tested

--- index.js
+++ index.js
@@ -25,12 +25,21 @@ allMovies = API.fetchAllMovies()
 console.log(allMovies)
 
 api.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
 api.get('/api/Movies', (req, res) => {
-    res.send(allMovies)
+  res.send(allMovies)
 })
+
+// Returns movies from specific genre
+api.get('/api/Movies/genre/:genre', (req, res) => {
+  const movie = allMovies.find((c) => c.genre === req.params.genre)
+  if (!movie)
+    return res.status(404).send('The movies with the given genre was not found')
+  res.send(movie)
+})
+
 
 const port = process.env.PORT || 3000
 api.listen(port, () => console.log(`Listening on ${port}...`))
--- test/movies.mjs
+++ test/movies.mjs
@@ -4,9 +4,17 @@ const request = supertest('http://localhost:3000/api/')
 import { expect } from 'chai'
 
 describe('Testing', () => {
+  // Testing the API
   it('GET /Movies', () => {
     return request.get(`Movies`).then((res) => {
       expect(res.body).to.not.be.empty
+    })
+  })
+
+  // Testing to get specific genre
+  it('GET /Movies/genre/:genre', () => {
+    return request.get(`Movies/genre/Action`).then((res) => {
+      expect(res.body.genre).to.be.eq('Action')
     })
   })
 })
